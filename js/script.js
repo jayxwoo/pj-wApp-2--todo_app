ffa22d4a182c08b00cae88bda2842c2c4d7e3476
JUNG-MIN WOO <jayxwoo@gmail.com>
2020-09-15 08:03:14 +0900

    // 'add-todo reader' class and 'main' function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,38 @@
 // ========== imports ==========
 import './default.js';
 
+// ========== script ==========
+// reference
+const addTodoForm = document.querySelector('.add-todo-form');
+
+// add-todo reader
+class AddTodoReader {
+    constructor(addTodoForm) {
+        this.addTodoForm = addTodoForm;
+    }
+
+    // read add-todo
+    read = function () {
+        return this.addTodoForm.add.value.trim();
+    }
+};
+
+// todo displayer
+
+// main
+const main = function () {
+    // instances
+    const addTodoReader = new AddTodoReader(addTodoForm);
+
+    addTodoForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        // addTodoReader
+        const addTodoText = addTodoReader.read();
+        console.log(addTodoText);
+
+        addTodoForm.reset();
+    });
+};
+
+main();
